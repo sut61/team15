04cb735beb580cdc87bae416ab80d8a2a3e14adc
ratthakan <ratthakanlovepang@gmail.com>
2019-02-09 17:35:51 +0700

    Edit Entity Payment -close #108

--- backend/src/main/java/com/example/lab/Entity/Payment.java
+++ backend/src/main/java/com/example/lab/Entity/Payment.java
@@ -3,6 +3,7 @@ package com.example.lab.Entity;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 import java.util.Date;
 
 @Data
@@ -19,19 +20,34 @@ public class Payment {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="payment_seq")
     @Column(name="Payment_ID",unique = true, nullable = true)
     private @NonNull Long pay_ID;
-    private @NonNull int payMent;
-    private @NonNull Date datePay;
+
+    @NotNull
+    @Min(value = 1)
+    @Max(value = 99999)
+    @Positive
+    private Integer payMent;
+    @NotNull
+    private  Date datePay;
+
+
+    @NotNull(message = " Phone must not be null to be valid")
+    @Size(min = 10,max = 10)
+    @Pattern(regexp = "^08([0-9])+")
+    private @NonNull String phoneNumber;
 
     @ManyToOne(fetch = FetchType.EAGER, targetEntity = Customer.class)
     @JoinColumn(name = "C_ID" ,insertable = true)
+    @NotNull
     private Customer customer;
 
     @ManyToOne(fetch = FetchType.EAGER, targetEntity = Type.class)
     @JoinColumn(name = "Type_ID",referencedColumnName="TYPE_ID",insertable = true)
+    @NotNull
     private Type nameType;
 
     @ManyToOne(fetch = FetchType.EAGER, targetEntity = DentistData.class)
     @JoinColumn(name = "Den_ID",insertable = true)
+    @NotNull
     private DentistData denname;
 
     public int getPayMent() {
@@ -72,5 +88,13 @@ public class Payment {
 
     public void setDenname(DentistData denname) {
         this.denname = denname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 }
